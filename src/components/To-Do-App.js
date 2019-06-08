eb1c2dcb7826c13f3cb8c3f947aeb1e54fcd73e1
rmt002 <roshanmthaliath@gmail.com>
2019-06-08 13:14:49 +0530

    Completed header

--- src/components/To-Do-App.js
+++ src/components/To-Do-App.js
@@ -1,15 +1,20 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import ToDoHeader from './To-Do-Header';
 import ToDoList from './To-Do-List';
 import ToDoEntry from './To-Do-Entry';
+import Card from '../../node_modules/react-bootstrap/Card';
 
-export class ToDoApp extends Component{
-    render(){
-        return(
+export class ToDoApp extends Component {
+    render() {
+        return (
             <div>
-                <ToDoHeader/>
-                <ToDoList />
-                <ToDoEntry/>
+                <Card>
+                    <Card.Body>
+                        <ToDoHeader />
+                        <ToDoList />
+                        <ToDoEntry />
+                    </Card.Body>
+                </Card>
             </div>
         )
     }
--- src/components/To-Do-Header.js
+++ src/components/To-Do-Header.js
@@ -1,10 +1,20 @@
-import React,{ Component } from 'react';
-
-export class ToDoHeader extends Component{
-    render(){
-        return(
+import React, { Component } from 'react';
+import Nav from '../../node_modules/react-bootstrap/Nav';
+export class ToDoHeader extends Component {
+    render() {
+        return (
             <div>
-                Add New
+                <Nav className="justify-content-end" activeKey="/home">
+                    <Nav.Item>
+                        <Nav.Link><button className="btn btn-primary">Add New Task</button></Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link ><button className="btn btn-secondary">Mark All Complete</button></Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link><button className="btn btn-danger">Clear All Completed</button></Nav.Link>
+                    </Nav.Item>
+                </Nav>
             </div>
         )
     }
