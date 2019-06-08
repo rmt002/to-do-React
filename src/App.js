eb37d30cec9e83f20b2d0ddf2b5188420cefb418
rmt002 <roshanmthaliath@gmail.com>
2019-06-08 12:19:36 +0530

    New React APP

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <div className="header">
+        <h2 >To-Do App</h2>
+      </div>
+      <div className="App">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
